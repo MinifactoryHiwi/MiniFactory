84de045f9e6e7dc8f921d147ff4e4938a49c9f62
George Katranis <georg.katranis@gmail.com>
2022-09-20 11:27:45 +0200

    New File added

---
+++ CodeBase/objecthandler.py
@@ -0,0 +1,8 @@
+"""
+Class for handling and creating objects (e.g. assigning ID) for the elements being processed on the factory. Intention
+is to enable controlling the usage of the actuators even more by blocking certain paths.
+"""
+
+
+class ObjectHandler:
+    pass
